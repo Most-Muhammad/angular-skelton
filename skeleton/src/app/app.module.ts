577bbb3f73f1795869059c59d1a9b8b607b44864
Mostafa <mostafa.elmostafa.m.elbanhawy@gmail.com>
2023-04-30 07:15:56 +0300

    Use UrlSerializer

--- skeleton/src/app/app.module.ts
+++ skeleton/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UrlSerializer } from '@angular/router';
+import { LowerCaseUrlSerializer } from '@core/classes/lower.case.url.serializer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+
+    {
+      provide: UrlSerializer,
+      useClass: LowerCaseUrlSerializer,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
